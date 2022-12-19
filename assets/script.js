c35483061ecd8849c5c5ecd90545214101e4725a
smithm4949 <smithm4949@gmail.com>
2022-12-19 17:19:32 -0500

    weather function working, styling needs a little more

---
+++ assets/script.js
@@ -0,0 +1,67 @@
+var searchButton = document.getElementById("search_button");
+var searchInput = document.getElementById("search_input");
+var cityTitle = document.getElementById("city_title");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+var multiDayWrapper = document.getElementById("5_day_wrapper");
+var buttonList = document.getElementById("previous_list");
+var city;
+
+async function searchAndGetWeather(searchTerm) {
+  let inputs = searchTerm.split(',');
+  city = {name: inputs[0].trim(), state:inputs[1].trim()};
+  //fetch coords from city name
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.name},${city.state},US&limit=1&appid=78025875a9c545a4d2f7c151cc462ba9
+  `)
+  .then((response) => response.json())
+  .then((data) => {
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=imperial&appid=78025875a9c545a4d2f7c151cc462ba9`)
+  })
+  .then((res) => res.json())
+  .then(data => updateDisplay(data.list));
+}
+
+function updateDisplay(weatherArray) {
+  multiDayWrapper.innerHTML = '';
+  let forecastTitle = document.createElement("h2")
+  forecastTitle.innerHTML = "5-day forecast";
+  multiDayWrapper.appendChild(forecastTitle);
+  for (let i = 0; i < (weatherArray.length - 1); i+=8) {
+    console.log(weatherArray[i])
+    let temp = weatherArray[i].main.temp;
+    let humidity = weatherArray[i].main.humidity;
+    let wind = weatherArray[i].wind.speed;
+    let date = weatherArray[i].dt_txt.split(' ',1)[0];
+
+    if (i === 0) {
+      cityTitle.innerText = `${city.name} ${date}`;
+      tempEl.innerText = `${temp} °F`;
+      windEl.innerText = `${wind} MPH`;
+      humidityEl.innerText = `${humidity} %`;
+    }
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "col");
+    card.innerText = `${date}, ${temp} °F ${humidity} % ${wind} MPH`;
+    multiDayWrapper.appendChild(card); 
+  }
+}
+
+function handleSearchClick() {
+  searchAndGetWeather(searchInput.value)
+
+  let recentButton = document.createElement("button");
+  recentButton.setAttribute("class", "row");
+  recentButton.innerHTML = searchInput.value;
+  buttonList.appendChild(recentButton);
+}
+
+function handleListClick(e) {
+  if (e.target.tagName === "BUTTON") {
+    searchAndGetWeather(e.target.innerHTML);
+  }
+}
+
+searchButton.addEventListener("click", handleSearchClick);
+buttonList.addEventListener("click", handleListClick);
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Challenge 06 - Weather Dashboard
+
+## Description
+
+This weeks challenge is to create a dashboard that shows the current weather and the 5-day forcast for a city that a user searchs.
+
+## Deployment
+
+This website is live and can be viewed at the following url:
+[https://smithm4949.github.io/weather_ch06/](https://smithm4949.github.io/weather_ch06/)
+
+## Example
+
+A screenshot of the app showing the weather is included below.:![The weather is shown for new york](./assets/weather%20screenshot.png)
+
+## Authors and acknowledgment
+
+This homework assignment was completed and developed by Myles Smith.
+
+## Project status
+
+This project is feature complete according to the acceptance criteria and there are currently no plans for further development.
